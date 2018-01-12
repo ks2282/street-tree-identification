af34929cc7074b245a139c9b63bbb8eb8d7c8d76
Kristie Sarkar <kristie.sarkar@gmail.com>
2018-01-12 14:53:11 -0800

    typo

--- src/model_training.py
+++ src/model_training.py
@@ -107,7 +107,7 @@ def nn_model(X_train, X_val, y_train, y_val, num_epochs, batch_size, image_color
     model.fit(X_train, y_train,
           verbose=1,
           batch_size = batch_size,
-          epochs=num_epochsy)
+          epochs=num_epochs)
 
     score = model.evaluate(X_val, y_val, verbose=0)
     print('Validation loss:' , score[0])
