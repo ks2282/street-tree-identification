e1eeb7cbebc3d0845d24e60fbe51ea0bea6d9f0d
Kristie Sarkar <kristie.sarkar@gmail.com>
2018-01-13 17:15:00 -0800

    adding dropout rate for vgg

--- src/model_training.py
+++ src/model_training.py
@@ -160,8 +160,10 @@ def vgg_model(X_train, X_val, y_train, y_val, num_epochs, batch_size, image_colo
         Conv2D(512, (3, 3), activation='relu', padding='same',),
         MaxPooling2D(pool_size=(2, 2), strides=(2, 2)),
         Flatten(),
-        #Dense(4096, activation='relu'),
-        #Dense(4096, activation='relu'),
+        Dense(4096, activation='relu'),
+        Dropout(0.5),
+        Dense(4096, activation='relu'),
+        Dropout(0.5),
         Dense(1, activation='sigmoid')
         ])
 
